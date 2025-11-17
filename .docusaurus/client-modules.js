24616c75b824d2dd270670e26669ed3c83f96f31
LupereneProker <LupereneProker@outlook.com>
2025-11-17 14:39:40 +0800

    Deploy website - based on 9fb5d6efbb9f976d2e4aa09a562e787ed261bb90

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Luper\\Desktop\\web\\fictional-engineering-docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Luper\\Desktop\\web\\fictional-engineering-docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Luper\\Desktop\\web\\fictional-engineering-docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Luper\\Desktop\\web\\fictional-engineering-docusaurus\\src\\css\\custom.css"),
-];
